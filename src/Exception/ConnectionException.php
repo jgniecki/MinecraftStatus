bc537897d0133c6876c682ba3710450f66a750e7
Jakub Gniecki <kubuspl@onet.eu>
2024-01-27 11:49:14 +0100

    Thrown when the connection to the resource is not established

---
+++ src/Exception/ConnectionException.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+/**
+ * @author Jakub Gniecki <kubuspl@onet.eu>
+ * @copyright
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace DevLancer\MinecraftStatus\Exception;
+
+class ConnectionException extends Exception
+{
+
+}
