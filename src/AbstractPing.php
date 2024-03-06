e3012b790bc2cf0afd1f4d8d00fe1078e3bd8e1d
Jakub Gniecki <kubuspl@onet.eu>
2024-03-06 09:49:48 +0100

    Added classes

---
+++ src/AbstractPing.php
@@ -0,0 +1,87 @@
+<?php declare(strict_types=1);
+/**
+ * @author Jakub Gniecki
+ * @copyright Jakub Gniecki <kubuspl@onet.eu>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+namespace DevLancer\MinecraftStatus;
+
+
+use DevLancer\MinecraftStatus\Exception\ConnectionException;
+use DevLancer\MinecraftStatus\Exception\NotConnectedException;
+use DevLancer\MinecraftStatus\Exception\ReceiveStatusException;
+
+/**
+ * Class AbstractPing
+ * @package DevLancer\MinecraftStatus
+ */
+abstract class AbstractPing extends AbstractStatus
+{
+    /**
+     * @inheritDoc
+     * @return AbstractPing
+     * @throws ConnectionException Thrown when failed to connect to resource
+     * @throws ReceiveStatusException Thrown when the status has not been obtained or resolved
+     */
+    public function connect(): self
+    {
+        if ($this->isConnected())
+            $this->disconnect();
+
+        $this->_connect($this->host, $this->port);
+        $this->getStatus();
+        return $this;
+    }
+
+    abstract protected function getStatus();
+
+    /**
+     * Copied from https://github.com/xPaw/PHP-Minecraft-Query/
+     *
+     * @return int
+     * @throws ReceiveStatusException
+     */
+    protected function readVarInt(): int
+    {
+        $i = 0;
+        $j = 0;
+
+        while(true) {
+            $k = @\fgetc($this->socket);
+            if($k === false)
+                return 0;
+
+            $k = \ord($k);
+            $i |= ($k&0x7F) << $j++ * 7;
+
+            if($j>5)
+                throw new ReceiveStatusException( 'VarInt too big' );
+
+            if(($k&0x80) != 128)
+                break;
+        }
+
+        return $i;
+    }
+
+    /**
+     * @return int
+     * @throws NotConnectedException
+     */
+    public function getCountPlayers(): int
+    {
+        return (int) ($this->getInfo()['players']['online'] ?? 0);
+    }
+
+    /**
+     * @return int
+     * @throws NotConnectedException
+     */
+    public function getMaxPlayers(): int
+    {
+        return (int) ($this->getInfo()['players']['max'] ?? 0);
+    }
+}
---
+++ src/AbstractQuery.php
@@ -0,0 +1,102 @@
+<?php declare(strict_types=1);
+/**
+ * @author Jakub Gniecki
+ * @copyright Jakub Gniecki <kubuspl@onet.eu>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+namespace DevLancer\MinecraftStatus;
+
+
+use DevLancer\MinecraftStatus\Exception\ConnectionException;
+use DevLancer\MinecraftStatus\Exception\NotConnectedException;
+use DevLancer\MinecraftStatus\Exception\ReceiveStatusException;
+
+/**
+ * Class AbstractQuery
+ * @package DevLancer\MinecraftStatus
+ */
+abstract class AbstractQuery extends AbstractStatus
+{
+    /**
+     * @inheritDoc
+     * @return AbstractQuery
+     * @throws ConnectionException Thrown when failed to connect to resource
+     * @throws ReceiveStatusException Thrown when the status has not been obtained or resolved
+     */
+    public function connect(): self
+    {
+        if ($this->isConnected())
+            $this->disconnect();
+
+        $this->_connect('udp://' . $this->host, $this->port);
+        \stream_set_blocking($this->socket, true);
+        $this->getStatus();
+        return $this;
+    }
+
+    /**
+     * @return int
+     * @throws NotConnectedException
+     */
+    public function getCountPlayers(): int
+    {
+        return (int) ($this->getInfo()['numplayers'] ?? 0);
+    }
+
+    /**
+     * @return int
+     * @throws NotConnectedException
+     */
+    public function getMaxPlayers(): int
+    {
+        return (int) ($this->getInfo()['maxplayers'] ?? 0);
+    }
+
+    /**
+     * Copied from https://github.com/xPaw/PHP-Minecraft-Query/
+     *
+     * @param int $command
+     * @param string $append
+     * @return string|null
+     * @throws ReceiveStatusException
+     */
+    protected function writeData(int $command, string $append = ""): ?string
+    {
+        $command = \pack('c*', 0xFE, 0xFD, $command, 0x01, 0x02, 0x03, 0x04) . $append;
+        $length  = \strlen($command);
+
+        if($length !== \fwrite($this->socket, $command, $length))
+            throw new ReceiveStatusException( "Failed to write on socket.");
+
+        $data = \fread($this->socket, 4096);
+
+        if($data === false)
+            throw new ReceiveStatusException( "Failed to read from socket.");
+
+        if(\strlen($data) < 5 || $data[0] != $command[2])
+            return null;
+
+        return \substr($data, 5);
+    }
+
+    /**
+     * Copied from https://github.com/xPaw/PHP-Minecraft-Query/
+     *
+     * @return string
+     * @throws ReceiveStatusException
+     */
+    protected function getChallenge(): string
+    {
+        $data = $this->writeData(0x09);
+
+        if(!$data)
+            throw new ReceiveStatusException('Failed to receive challenge.');
+
+        return \pack('N', $data);
+    }
+
+    abstract protected function getStatus(): void;
+}
