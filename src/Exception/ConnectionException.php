cceb96950b1c6ea9f13e562008a01c86c912e6e9
jgniecki <kubuspl@onet.eu>
2026-05-31 01:18:35 +0200

    Update the author contact email

--- src/Exception/ConnectionException.php
+++ src/Exception/ConnectionException.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 /**
- * @author Jakub Gniecki <kubuspl@onet.eu>
+ * @author Jakub Gniecki <jgniecki.contact@gmail.com>
  * @copyright
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
--- src/Exception/Exception.php
+++ src/Exception/Exception.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types=1);
 /**
  * @author Jakub Gniecki
- * @copyright Jakub Gniecki <kubuspl@onet.eu>
+ * @copyright Jakub Gniecki <jgniecki.contact@gmail.com>
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
